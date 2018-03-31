f8f0e16a7796f9a38d8acd1d8aac002a0d6ab13f
Joaaofiilho <joaot10112@gmail.com>
2018-03-31 16:22:56 -0300

    Terminado os comandos base do controller da tela principal

--- src/view/Controllers/TelaPrincipal.java
+++ src/view/Controllers/TelaPrincipal.java
@@ -3,6 +3,7 @@ package view.Controllers;
 import beans.Paciente;
 import beans.Consulta;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -28,5 +29,36 @@ public class TelaPrincipal {
     public void initialize(){
         tglConsulta.setSelected(true);
         lstViewLista = new ListView<Consulta>();
+    }
+
+    //Controle do dia
+    public void btnDireitaOnAction(ActionEvent event){
+
+    }
+
+    public void btnEsquerdaOnAction(ActionEvent event){
+
+    }
+
+    //Controle lista lateral
+    public void tglPacienteOnAction(ActionEvent event){
+
+    }
+
+    public void tglConsultaOnAction(ActionEvent event){
+
+    }
+
+    //Lista lateral
+    public void btnAdicionarOnAction(ActionEvent event){
+
+    }
+
+    public void btnModificarOnAction(ActionEvent event){
+
+    }
+
+    public void btnRemoverOnAction(ActionEvent event){
+
     }
 }
