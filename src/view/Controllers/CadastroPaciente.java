99eb39156a0c997697c20e68ec487487dcc5757f
Joaaofiilho <joaot10112@gmail.com>
2018-03-31 16:44:13 -0300

    Interligado o controller da tela Cadastro Paciente com os elementos da tela.

--- src/view/Controllers/CadastroPaciente.java
+++ src/view/Controllers/CadastroPaciente.java
@@ -2,11 +2,26 @@ package view.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 public class CadastroPaciente {
     public TextField txtfieldNome;
-    //oi
-    //rs
+    public TextField txtfieldTelefone;
+    public TextField txtfieldEmail;
+    public TextField txtfieldCPF;
+    public DatePicker dateNascimento;
+    public RadioButton rdBtnMasculino;
+    public RadioButton rdBtnFeminino;
+
     public TextField txtFieldLogradouro;
+
+    public void rdBtnMasculinoOnAction(ActionEvent event){
+
+    }
+
+    public void rdBtnFemininoOnAction(ActionEvent event){
+
+    }
 }
