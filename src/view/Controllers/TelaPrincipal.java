42ed3b9035d815034badb455f2d76149d13dd073
Joaaofiilho <joaot10112@gmail.com>
2018-03-31 15:52:42 -0300

    Controller da tela principal devidamente linkado com os botoes da tela.

--- src/view/Controllers/TelaPrincipal.java
+++ src/view/Controllers/TelaPrincipal.java
@@ -1,10 +1,32 @@
 package view.Controllers;
 
+import beans.Paciente;
+import beans.Consulta;
+
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.shape.Rectangle;
 
 public class TelaPrincipal {
+    //Controle do dia
     public Label lblDia;
-    public Rectangle recContainerDia;
+    public Button btnEsquerda;
+    public Button btnDireita;
 
+    //Controle lista lateral
+    public ToggleButton tglPaciente;
+    public ToggleButton tglConsulta;
+
+    //Lista lateral
+    public ListView lstViewLista;
+    public Button btnAdicionar;
+    public Button btnModificar;
+    public Button btnRemover;
+
+    public void initialize(){
+        tglPaciente.setSelected(true);
+        lstViewLista = new ListView<Paciente>();
+    }
 }
