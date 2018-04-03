d5ad57e6f336ca974abea697b4e596a8460aec0e
Rubens Diniz <rubensadiniz@edu.unifor.br>
2018-04-03 10:27:25 -0300

    CadastroConsulta organizado

--- src/view/Controllers/CadastroConsulta.java
+++ src/view/Controllers/CadastroConsulta.java
@@ -1,5 +1,32 @@
 package view.Controllers;
 
-public class CadastroConsulta {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
+public class CadastroConsulta {
+    //Textfields
+    public TextField txtfieldDescricao;
+    public TextField txtfieldValor;
+    public TextField txtfieldHorario;
+
+    //Datepicker
+    public DatePicker dateData;
+
+    //Combobox
+    public ComboBox<String> cbPacientes;
+
+    //Botoes
+    public Button btnVoltar;
+    public Button btnSalvar;
+
+    public void btnVoltarOnAction(ActionEvent e){
+
+    }
+
+    public void btnSalvarOnAction(ActionEvent e){
+
+    }
 }
