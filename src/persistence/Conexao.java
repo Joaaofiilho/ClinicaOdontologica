d40da0c28ac63e8f18fba4ed0bbecb59d972363a
Joao <joaot10112@gmail.com>
2018-05-15 14:21:07 -0300

    Criada a classe de Conexao

---
+++ src/persistence/Conexao.java
@@ -0,0 +1,11 @@
+package persistence;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:mysql://localhost/bd_prova1?useTimezone=true&serverTimezone=UTC-3","root","root");
+    }
+}
