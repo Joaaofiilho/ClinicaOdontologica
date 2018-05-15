74427a2daf90acf33441b7791d3ad8c8ff02a8f8
Joao <joaot10112@gmail.com>
2018-05-15 15:32:40 -0300

    Adicionada a Library do SQL

--- src/persistence/Conexao.java
+++ src/persistence/Conexao.java
@@ -6,6 +6,6 @@ import java.sql.SQLException;
 
 public class Conexao {
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost/ClinicaOdontologica-bd?useTimezone=true&serverTimezone=UTC-3","root","root");
+        return DriverManager.getConnection("jdbc:mysql://localhost/ClinicaOdontologica-bd?useTimezone=true&serverTimezone=UTC-3","root","12345678");
     }
 }
